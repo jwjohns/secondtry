9c6fcc3cddcdda9cff0af36d0fbb5161e7f84cdf
jwjohns <justin@lucienconsulting.com>
2016-01-27 22:19:26 -0500

    adding comments"

--- index.php
+++ index.php
@@ -1,42 +1,87 @@
 <html> 
-
-
+	<head>
+		<!-- set base viewport -->
+		<meta name="viewport" content="initial-scale=1.0">
+	    <meta charset="utf-8">
+	    <!-- set mandated styles for the map to display -->
+	    <!-- set styles for the floating input panel -->
+	    <style>
+		    html, body {
+		      height: 100%;
+		      margin: 0;
+		      padding: 0;
+		    }
+		    #map {
+		      height: 100%;
+		    }
+		
+			#floating-panel {
+			  position: absolute;
+			  top: 10px;
+			  left: 25%;
+			  z-index: 5;
+			  background-color: #fff;
+			  padding: 5px;
+			  border: 1px solid #999;
+			  text-align: center;
+			  font-family: 'Roboto','sans-serif';
+			  line-height: 30px;
+			  padding-left: 10px;
+			}
+		</style>
+	<!-- iclude maps api -->
+	<script src="https://maps.googleapis.com/maps/api/js"></script>
+	<!-- include jquery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<script type="text/javascript">
+	<script type="text/javascript">
 			
-		    //When the Window finishes loading...
-		    $(document).ready(function () {
-		    	$("#contactForm").on('submit', function (e) {
+	    // When the Window finishes loading...
+	    $(document).ready(function () {
+	    	$("#contactForm").on('submit', function (e) {
 		    	e.preventDefault()
+		    	// set the form data equal to data var
 		    	var data = new FormData(this);
 		        //Carry out an Ajax request.
 		        $.ajax({
+		        	// upload URL
 		            url: 'upload.php',
+		            // method
 		            method: 'POST',
-		            async: false,
-		            cache: false,
+		            // DO NOT CACHE THE CALL
+		            cache: false, 
+		            // mixed content
 		            contentType: false,
+		            // dont let jquery handle as string
 		            processData: false,
+		            // set data equal to data for the ajax request
 		            data: data,
 		            success:function(data){
 		                //Loop through each location.
 		                console.log(data)
+		                // write dev data to data div
+		                $('#dump').text(data)
+		                // loop over data
 		                $.each(data, function(){
 
-		                    //Plot the location as a marker
-		                    var pos = new google.maps.LatLng(this.lat, this.lng); 
-		                    new google.maps.Marker({
-		                        position: pos,
-		                        map: map
-		                    });
-		                });
-		            }
-		        });
-		      });
+		      // DATA NEEDS TO BE HANDLED IN UPLOAD.PHP AND RETURNED IN A FORMAT FOR MAPS API
 
-		    });
+		      			// var marker = new MapMarker(optionsObject);
 
-		</script>
+
+		                   //Plot the location as a marker
+		                    // var pos = new google.maps.LatLng(this.lat, this.lng); 
+		                    // new google.maps.Marker({
+		                    //     position: pos,
+		                    //     map: map
+		                    // });
+	                });
+	            }
+	        });
+	    });
+
+	});
+
+	</script>
 
 	</head>
 		<div id="data">
@@ -44,26 +89,51 @@
 		</div>
 
 	<body>
-
-		<div> 
+		<!-- set the input form to be a floating panel -->
+		<div id="floating-panel"> 
+			<!-- no action needed as its ajax -->
 			<form method="POST" id="contactForm" enctype="multipart/form-data">
+				<!-- set the upload file -->
+			<p> please choose a file to upload and input the column names below</p>
 			<input type="file" id="csv" name="csv" />
-			
-			<hr />
-			<p> please input the column names.</p>
+			<!-- my attempt at getting the column names, this was another interesting task, mostly we made the csv format standard and with python there is a method to load it in which you can set the keys extremly easily -->
 			<input type="text" name="col1" value="test" />
 			<input type="text" name="col2" value="test" />	
 			<input type="text" name="col3" value="test" />
 			<input type="text" name="col4" value="test" />
 			<input type="text" name="col5" value="test" />
 
-			<input type="submit" id="submitBtn" name="submit" value="Save" /></form>
+			<input type="submit" id="submitBtn" name="submit" value="Upload" /></form>
 		</div>
+		
 		<hr />
-		<div id="map"></div>
-    	<?php var_dump($_FILES); ?>
-    	
-    	<?php phpinfo(); ?>
+
+		<!-- DIV FOR DEVELOPMENT DUMP OF RETURN DATA FROM UPLOAD.PHP -->
+		<div id="dump"> </div>
+		
+		<hr />
+		
+			<!-- SET MAPS DIV -->
+			<div id="map"></div>
+		
+		 <script>
+		// Initialize the actual map itself
+	    var map = new google.maps.Map(document.getElementById('map'), {
+	    	//CENTER BRECKENRIDGE COLORADO (MY FAVORITE PLACE!)
+
+	    	//I am interested to discuss the way to center the map when not knowing an avg 
+	    	//of the locations to be uploaded. I have ideas but suspect an easish way you may 
+	    	//know of.
+
+
+	    	// center marker on first location with a wide zoom? 
+	      center: {lat: 39.500227, lng: -106.0431},
+	      zoom: 15
+	    });
+
+	    </script>
+
+
 	</body>
 
 
--- upload.php
+++ upload.php
@@ -1,8 +1,84 @@
 <?php 
+// set return header
 header('Content-Type: applicaton/json');
+// get csv file from tmp upload dir
 $csv = $_FILES['csv']['tmp_name'];
+// parse csv into array
 $csvAsArray = array_map('str_getcsv', file($csv));
+// return the json array
+// COUNT THE ARRAY
+// $counted = array_count_values($csvAsArray);
 
-echo json_encode($csvAsArray); 
+// set the gps array for post geocoding
+$gps = [];
+
+// loop over addresses and get the geocode lat long
+foreach ($csvAsArray as $key => $value) {
+	// implode the address into one string
+	$address = implode(" ",$value);
+	// send to google maps geocode api
+	// flawed as right now the HEX color code is on the end of the scrip
+	$coords = getLatLong($address);
+	// add the return values to the $gps array for return to the initial page
+	array_push($gps, $coords );
+}
+// return the values to the index.php page
+echo json_encode($gps);
+
+//echo json_encode($csvAsArray); 
+
+// TODO
+
+// GEOCODE ADDRESSES IN THE LOOP LIKELY IN THIS PAGE
+
+// RETURN DATA FORMATTED DIRECTLY FOR MAP MARKERS FROM THIS PAGE
+// function to geocode address, it will return false if unable to geocode address
+
+// function to get the lat long from the addresses
+function getLatLong($address){
+ 
+    // url encode the address
+    $address = urlencode($address);
+     
+    // google map geocode api url
+    $url = "http://maps.google.com/maps/api/geocode/json?address={$address}";
+ 
+    // get the json response
+    $resp_json = file_get_contents($url);
+     
+    // decode the json
+    $resp = json_decode($resp_json, true);
+ 
+    // response status will be 'OK', if able to geocode given address 
+    if($resp['status']=='OK'){
+ 
+        // get the important data
+        $lati = $resp['results'][0]['geometry']['location']['lat'];
+        $longi = $resp['results'][0]['geometry']['location']['lng'];
+        $formatted_address = $resp['results'][0]['formatted_address'];
+         
+        // verify if data is complete
+        if($lati && $longi && $formatted_address){
+         
+            // put the data in the array
+            $data_arr = array();            
+             
+            array_push(
+                $data_arr, 
+                    $lati, 
+                    $longi, 
+                    $formatted_address
+                );
+             
+            return $data_arr;
+             
+        }else{
+            return false;
+        }
+         
+    }else{
+        return false;
+    }
+}
 
 ?>
